f1543bb214d122b9d02ffb06c8e06722e3d16cec
Carolina <barbozacaro@gmail.com>
2021-12-16 10:54:26 -0300

    [update] styles

--- carolina/interactive-pricing-component/tailwind.config.js
+++ carolina/interactive-pricing-component/tailwind.config.js
@@ -7,13 +7,19 @@ module.exports = {
       lilac: '#F1F5FE',
       purple: '#293356',
       grey: '#848EAD',
+      lightGrey: '#ECF0FB',
       green: '#10D8C4',
-      white: '#FFFFFF'
+      lightGreen: '#A4F3EB',
+      darkGreen: '#24AEA1',
+      white: '#FFFFFF',
+      lightPurple: '#CFD8EF',
+      lightOrange: '#FEEDE8',
+      darkOrange: '#FF8D68',
     },
     extend: {
       fontFamily: {
         sans: 'manrope',
-      }
+      },
     },
   },
   variants: {
